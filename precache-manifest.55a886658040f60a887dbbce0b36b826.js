7cd1604c65139e2c154c522a1551dd258e7cdf73
guram21@gmail.com <nibmub-qEqpag-qijzy1>
2020-02-15 15:03:49 -0500

    Updates

---
+++ precache-manifest.55a886658040f60a887dbbce0b36b826.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fbc248018c8673294dc4e0b2121d1ea",
+    "url": "/cra1/index.html"
+  },
+  {
+    "revision": "1ce443f658edc23ec341",
+    "url": "/cra1/static/css/main.169c7dfb.chunk.css"
+  },
+  {
+    "revision": "bb0f2bb7e6f6c0cbeca9",
+    "url": "/cra1/static/js/2.79d9e068.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cra1/static/js/2.79d9e068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce443f658edc23ec341",
+    "url": "/cra1/static/js/main.e8175b67.chunk.js"
+  },
+  {
+    "revision": "358fadb87d4716f3bbce",
+    "url": "/cra1/static/js/runtime-main.04104b05.js"
+  },
+  {
+    "revision": "864297ca2959f6e1dbd5064d3804229a",
+    "url": "/cra1/static/media/logo.864297ca.svg"
+  }
+]);
